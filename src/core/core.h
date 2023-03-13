685fdc3eed203f0e9aa24c436bab4613825d9bd7
rama <i@rrmstu.ru>
2023-03-13 21:46:10 +0500

    :sparkles: :: add header function catch error, check: binary, digit, init

---
+++ src/core/core.h
@@ -0,0 +1,38 @@
+//
+// Created by rama on 13.03.23.
+//
+
+#ifndef S21_SMARTCALC_V1_0_CORE_H
+#define S21_SMARTCALC_V1_0_CORE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "ds.h"
+
+#define SIZE 255
+
+typedef enum {
+    OK,
+    INVALID,
+    MEM,
+    CALC
+} _logger;
+
+char *rpn(char *in, int *err);
+
+void _catch_err(char *in, int *err);
+
+int is_binary(char *ch);
+
+int is_digit(char *ch);
+
+int is_unary(char *ch);
+
+
+int get_operation_priority(char _operation);
+
+
+#endif //S21_SMARTCALC_V1_0_CORE_H
