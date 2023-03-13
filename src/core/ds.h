1893459143efce344a6819b19d697ad36a72e47b
rama <i@rrmstu.ru>
2023-03-13 15:55:37 +0500

    [:sparkles:] add header function for stack manager -> init, push, pop, peek

---
+++ src/core/ds.h
@@ -0,0 +1,45 @@
+//
+// Created by rama on 13.03.23.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#ifndef S21_SMARTCALC_V1_0_DS_H
+#define S21_SMARTCALC_V1_0_DS_H
+
+/*
+ * @brief
+ */
+typedef struct operator_stack {
+    char val;
+    struct operator_stack *next;
+} operator_stack;
+
+/*
+ * @brief
+ */
+typedef struct operand_stack {
+    double val;
+    struct operand_stack *next;
+} operand_stack;
+
+struct operator_stack init_operator_stack(char operator);
+
+struct operand_stack init_operand_stack(double operand);
+
+size_t get_size_operator_stack(operator_stack *head);
+
+operator_stack *push_operator_stack(operator_stack *head, char data);
+
+operand_stack *push_operand_stack(operand_stack *head, double operand);
+
+operator_stack *pop_operator_stack(operator_stack *head);
+
+operand_stack *pop_operand_stack(operand_stack *head);
+
+char peek_operator_stack(operator_stack *head);
+
+double peek_operand_stack(operand_stack *head);
+
+#endif //S21_SMARTCALC_V1_0_DS_H
